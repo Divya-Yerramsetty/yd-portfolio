a73e86a220662180835a41df2692eb9bc41c536b
Divya Yerramsetty <divya@Divyas-MBP.attlocal.net>
2023-04-03 07:19:53 -0500

    minor update

--- src/components/Education.js
+++ src/components/Education.js
@@ -12,7 +12,7 @@ export default function Education() {
                 </span>
                 <h3 className="mb-2 text-3xl font-semibold text-pink-600 dark:text-white">Masters in Information Science</h3>
                 <time className="block text-lg mb-2 text-lg font-normal leading-none text-black-400 dark:text-black-500">Graduated in May, 2018</time>
-                <p className="text-base text-lg font-bold text-black-500 dark:text-black-400">University of North Texas</p>
+                <p className="text-base text-lg font-bold text-black-500 dark:text-black-400">University of North Texas, Denton, TX.</p>
             </li>
             <li className="ml-6">
                 <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
@@ -20,7 +20,7 @@ export default function Education() {
                 </span>
                 <h3 className="mb-2 text-3xl font-semibold text-pink-600 dark:text-white">Bachelors in Mechanical Engineering</h3>
                 <time className="block text-lg mb-2 text-lg font-normal leading-none text-black-400 dark:text-black-500">Graduated in May, 2013</time>
-                <p className="text-base text-lg font-bold text-black-500 dark:text-black-400">Institute of Aeronautical Engineering affiliated to JNTUH</p>
+                <p className="text-base text-lg font-bold text-black-500 dark:text-black-400">Institute of Aeronautical Engineering affiliated to JNTUH, Hyderabad, India.</p>
             </li>
           </ol>
         </div>
