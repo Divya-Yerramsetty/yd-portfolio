857bf667fa9d4f04a7e50337e0480966c1a45f55
Divya Yerramsetty <divya@Divyas-MBP.attlocal.net>
2023-04-06 21:09:16 -0500

    styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,17 +6,17 @@ export default function About() {
     <section id="about" className="h-screen">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 text-center">
-          <h1 className="title-font sm:text-4xl text-4xl px-2 mb-2 font-medium text-pink-600 overflow-hidden lg:whitespace-nowrap animate-typing md:animate-typing">
+          <h1 className="title-font sm:text-4xl text-4xl px-4 mb-2 font-medium text-pink-600 overflow-hidden lg:whitespace-nowrap animate-typing md:animate-typing">
             Hi, I'm <span className="font-bold">Divya Yerramsetty.</span>
           </h1>
-          <h1 className="title-font sm:text-4xl text-4xl px-2 mb-4 font-medium text-pink-600 animate-fade-in-left delay-2000 duration-2000">
+          <h1 className="title-font sm:text-4xl text-4xl px-4 mb-4 font-medium text-pink-600 animate-fade-in-left delay-2000 duration-2000">
             I love to build amazing web apps.
           </h1>
           
-          <p className="mb-8 px-2 leading-relaxed text-2xl font-medium animate-fade-in-left">
+          <p className="mb-8 px-4 leading-relaxed text-2xl font-medium animate-fade-in-left">
             User Interface (UI) / Full Stack Developer | Experience in Insurance | E-Commerce | Travel booking apps | Energy | Fin-Tech Domains
           </p>
-          <p className="mb-8 px-2 leading-relaxed text-xl font-medium animate-fade-in-left">
+          <p className="mb-8 px-4 leading-relaxed text-xl font-medium animate-fade-in-left">
             Total IT Experience - Around 9 yrs | US Experience - 6 yrs | Located in Texas (Open to Relocate)
           </p>
           <div className="flex justify-center">
